75b35a46db3142eb9a820e6cbc85ff19ff929fdb
Matle Langosz <matle.langosz@me.com>
2024-05-22 10:35:04 +0200

    Update overrides and layout

--- overrides.rb
+++ overrides.rb
@@ -19,24 +19,13 @@ Autobuild::Package.each do |name, pkg|
     end
 end
 
-Autobuild::Package['planning/orogen/area_exploration'].define "CMAKE_BUILD_TYPE", "Debug"
-
-# Autobuild::Package['slam/cloud_slam_connector'].define "CMAKE_BUILD_TYPE", "Debug"
-
-Autoproj.manifest.each_autobuild_package do |pkg|
-    if pkg.kind_of?(Autobuild::Orogen)
-        pkg.orogen_options << '--extensions=textlog'
-        pkg.depends_on 'tools/orogen_textlog'
-    end
-end
-
 
 # enable this to build with qt5 (also enable the overrides from overrides.d/99-qt5.yml)
 # run "autopronj envsh" and ". env.sh" afterwards
-Autoproj.env_add "USE_QT5", "1"
+#Autoproj.env_add "USE_QT5", "1"
 
-# has to be set on ALL libs to actually work for the task
-Autobuild::Package['planning/area_exploration'].define "ENABLE_DEBUG_DRAWINGS", "ON"
-Autobuild::Package['planning/traversability_generator3d'].define "ENABLE_DEBUG_DRAWINGS", "ON"
-Autobuild::Package['planning/ugv_nav4d'].define "ENABLE_DEBUG_DRAWINGS", "ON"
-Autobuild::Package['planning/orogen/area_exploration'].define "ENABLE_DEBUG_DRAWINGS", "ON"
+# # has to be set on ALL libs to actually work for the task
+# Autobuild::Package['planning/area_exploration'].define "ENABLE_DEBUG_DRAWINGS", "ON"
+# Autobuild::Package['planning/traversability_generator3d'].define "ENABLE_DEBUG_DRAWINGS", "ON"
+# Autobuild::Package['planning/ugv_nav4d'].define "ENABLE_DEBUG_DRAWINGS", "ON"
+# Autobuild::Package['planning/orogen/area_exploration'].define "ENABLE_DEBUG_DRAWINGS", "ON"
